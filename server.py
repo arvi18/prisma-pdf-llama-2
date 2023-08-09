a0202c4b46b91c763e8e2fe4b4d076a7a9cd0111
arvi18 <arvind.shelke@devdynamics.ai>
2023-08-09 18:16:10 +0530

    source docs removed from reponse

--- server.py
+++ server.py
@@ -1,3 +1,11 @@
+"""
+This module contains a Flask server that provides an API endpoint for a chatbot. The chatbot is implemented using the
+`qa_bot` and `final_result` functions from the `model` module. The server listens for POST requests to the `/chat` endpoint
+and expects a JSON payload with a `query` field. The server responds with a JSON payload containing the original query,
+the chatbot's response, and a message indicating that no source documents are available. The server can be started by
+running this module directly.
+"""
+
 from flask import Flask, request, jsonify
 import langchain
 from model import qa_bot, final_result
@@ -14,12 +22,21 @@ def chat():
     print(query)
 
     response = final_result(query)
-    # return jsonify({'response': response})
+    response["source_documents"] = "No source documents available"
     return response
+
 
 if __name__ == '__main__':
     app.run()
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+# sample request
+# {
+#   "query": "What is the capital of France?"
+# }
+
+# sample response
+# {
+#   "query": "What is the capital of France?",
+#   "result": "Tardive dyskinesia, often known as “Bartholin’s cyst,” can be a severe side effect of certain antipsychotic medications. The condition occurs when glands (small ovarian-like organs) in the vulva become enlarged and then painful and uncomfortable to the patient.\n\nThe Bartholin’s gland cysts are commonly found in women of reproductive age, developing in approximately 2% of all women. The condition is caused by a blockage within the ovarian-like organs. These cysts and abscess-es may show up on mammograms or ultrasounds for patients who have this condition.\n\nTreatment\nIf you are diagnosed with Bartholin’s gland cysts, your doctor will likely prescribe antibiotics to treat the infection. This medication is usually taken orally and can be taken alongside other medications if necessary. If the antibiotic does not work, surgery may be required.\n\nPotential Risk Factors\nThe most common risk factor for developing a Bartholin’s gland cyst is having",
+#   "source_documents": ""
+# }
